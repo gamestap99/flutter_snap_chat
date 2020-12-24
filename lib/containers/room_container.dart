5b151a451f3feac08086c649fe8c165b4378dcec
Nguyễn Hoàng Tuấn <hoangtuan7319@gmail.com>
2020-12-25 00:34:50 +0700

    add container loose

--- lib/containers/room_container.dart
+++ lib/containers/room_container.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_snap_chat/blocs/chat_bloc/chat_cubit.dart';
-import 'package:flutter_snap_chat/repositories/chat_repository.dart';
-import 'package:flutter_snap_chat/screen_display/chat_display_screen.dart';
-import 'package:flutter_snap_chat/widget/bottom_navigate.dart';
+import 'package:flutter_snap_chat/blocs/authentication_bloc/bloc.dart';
 
-class ChatContainer extends StatelessWidget {
+import 'package:flutter_snap_chat/blocs/room_bloc/room_bloc.dart';
+import 'package:flutter_snap_chat/repositories/room_repository.dart';
+import 'package:flutter_snap_chat/screen_display/room_display_screen.dart';
+
+
+class RoomContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Map<String, dynamic> args = ModalRoute.of(context).settings.arguments;
-    print(args["userId"].toString());
+    String uid = context.select((AuthenticationBloc bloc) => bloc.state.user.id.toString());
+    print("uid" + uid);
     return BlocProvider(
-      create: (_) => ChatCubit(repository: ApiRoomRepository(), uid: args["userId"].toString()),
-      child: ChatDisplayScreen(),
+      create: (_) => RoomBloc(ApiRoomRepository(), uid),
+      child: RoomDisplayScreen(uid: uid,),
     );
   }
 }
--- lib/settings.dart
+++ lib/settings.dart
@@ -19,7 +19,7 @@ class ChatSettings extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'SETTINGS',
+          'Thông tin hiển thị',
           style: TextStyle(color: primaryColor, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
@@ -138,156 +138,158 @@ class SettingsScreenState extends State<SettingsScreen> {
     return Stack(
       children: <Widget>[
         SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              // Avatar
-              Container(
-                child: Center(
-                  child: Stack(
-                    children: <Widget>[
-                      (avatarImageFile == null)
-                          ? (photoUrl != ''
-                              ? Material(
-                                  child: CachedNetworkImage(
-                                    placeholder: (context, url) => Container(
-                                      child: CircularProgressIndicator(
-                                        strokeWidth: 2.0,
-                                        valueColor:
-                                            AlwaysStoppedAnimation<Color>(
-                                                themeColor),
+          child: Form(
+            child: Column(
+              children: <Widget>[
+                // Avatar
+                Container(
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        (avatarImageFile == null)
+                            ? (photoUrl != ''
+                                ? Material(
+                                    child: CachedNetworkImage(
+                                      placeholder: (context, url) => Container(
+                                        child: CircularProgressIndicator(
+                                          strokeWidth: 2.0,
+                                          valueColor:
+                                              AlwaysStoppedAnimation<Color>(
+                                                  themeColor),
+                                        ),
+                                        width: 90.0,
+                                        height: 90.0,
+                                        padding: EdgeInsets.all(20.0),
                                       ),
+                                      imageUrl: photoUrl,
                                       width: 90.0,
                                       height: 90.0,
-                                      padding: EdgeInsets.all(20.0),
+                                      fit: BoxFit.cover,
                                     ),
-                                    imageUrl: photoUrl,
-                                    width: 90.0,
-                                    height: 90.0,
-                                    fit: BoxFit.cover,
-                                  ),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(45.0)),
-                                  clipBehavior: Clip.hardEdge,
-                                )
-                              : Icon(
-                                  Icons.account_circle,
-                                  size: 90.0,
-                                  color: greyColor,
-                                ))
-                          : Material(
-                              child: Image.file(
-                                avatarImageFile,
-                                width: 90.0,
-                                height: 90.0,
-                                fit: BoxFit.cover,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(45.0)),
+                                    clipBehavior: Clip.hardEdge,
+                                  )
+                                : Icon(
+                                    Icons.account_circle,
+                                    size: 90.0,
+                                    color: greyColor,
+                                  ))
+                            : Material(
+                                child: Image.file(
+                                  avatarImageFile,
+                                  width: 90.0,
+                                  height: 90.0,
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(45.0)),
+                                clipBehavior: Clip.hardEdge,
                               ),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(45.0)),
-                              clipBehavior: Clip.hardEdge,
-                            ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.camera_alt,
-                          color: primaryColor.withOpacity(0.5),
+                        IconButton(
+                          icon: Icon(
+                            Icons.camera_alt,
+                            color: primaryColor.withOpacity(0.5),
+                          ),
+                          onPressed: getImage,
+                          padding: EdgeInsets.all(30.0),
+                          splashColor: Colors.transparent,
+                          highlightColor: greyColor,
+                          iconSize: 30.0,
                         ),
-                        onPressed: getImage,
-                        padding: EdgeInsets.all(30.0),
-                        splashColor: Colors.transparent,
-                        highlightColor: greyColor,
-                        iconSize: 30.0,
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
+                  width: double.infinity,
+                  margin: EdgeInsets.all(20.0),
                 ),
-                width: double.infinity,
-                margin: EdgeInsets.all(20.0),
-              ),
 
-              // Input
-              Column(
-                children: <Widget>[
-                  // Username
-                  Container(
-                    child: Text(
-                      'Nickname',
-                      style: TextStyle(
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.bold,
-                          color: primaryColor),
-                    ),
-                    margin: EdgeInsets.only(left: 10.0, bottom: 5.0, top: 10.0),
-                  ),
-                  Container(
-                    child: Theme(
-                      data: Theme.of(context)
-                          .copyWith(primaryColor: primaryColor),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Sweetie',
-                          contentPadding: EdgeInsets.all(5.0),
-                          hintStyle: TextStyle(color: greyColor),
-                        ),
-                        controller: controllerNickname,
-                        onChanged: (value) {
-                          nickname = value;
-                        },
-                        focusNode: focusNodeNickname,
+                // Input
+                Column(
+                  children: <Widget>[
+                    // Username
+                    Container(
+                      child: Text(
+                        'Nickname',
+                        style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold,
+                            color: primaryColor),
                       ),
+                      margin: EdgeInsets.only(left: 10.0, bottom: 5.0, top: 10.0),
                     ),
-                    margin: EdgeInsets.only(left: 30.0, right: 30.0),
-                  ),
-
-                  // About me
-                  Container(
-                    child: Text(
-                      'About me',
-                      style: TextStyle(
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.bold,
-                          color: primaryColor),
-                    ),
-                    margin: EdgeInsets.only(left: 10.0, top: 30.0, bottom: 5.0),
-                  ),
-                  Container(
-                    child: Theme(
-                      data: Theme.of(context)
-                          .copyWith(primaryColor: primaryColor),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Fun, like travel and play PES...',
-                          contentPadding: EdgeInsets.all(5.0),
-                          hintStyle: TextStyle(color: greyColor),
+                    Container(
+                      child: Theme(
+                        data: Theme.of(context)
+                            .copyWith(primaryColor: primaryColor),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Sweetie',
+                            contentPadding: EdgeInsets.all(5.0),
+                            hintStyle: TextStyle(color: greyColor),
+                          ),
+                          controller: controllerNickname,
+                          onChanged: (value) {
+                            nickname = value;
+                          },
+                          // focusNode: focusNodeNickname,
                         ),
-                        controller: controllerAboutMe,
-                        onChanged: (value) {
-                          aboutMe = value;
-                        },
-                        focusNode: focusNodeAboutMe,
                       ),
+                      margin: EdgeInsets.only(left: 30.0, right: 30.0),
                     ),
-                    margin: EdgeInsets.only(left: 30.0, right: 30.0),
-                  ),
-                ],
-                crossAxisAlignment: CrossAxisAlignment.start,
-              ),
 
-              // Button
-              Container(
-                child: FlatButton(
-                  onPressed: handleUpdateData,
-                  child: Text(
-                    'UPDATE',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  color: primaryColor,
-                  highlightColor: Color(0xff8d93a0),
-                  splashColor: Colors.transparent,
-                  textColor: Colors.white,
-                  padding: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
+                    // About me
+                    Container(
+                      child: Text(
+                        'About me',
+                        style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold,
+                            color: primaryColor),
+                      ),
+                      margin: EdgeInsets.only(left: 10.0, top: 30.0, bottom: 5.0),
+                    ),
+                    Container(
+                      child: Theme(
+                        data: Theme.of(context)
+                            .copyWith(primaryColor: primaryColor),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Fun, like travel and play PES...',
+                            contentPadding: EdgeInsets.all(5.0),
+                            hintStyle: TextStyle(color: greyColor),
+                          ),
+                          // controller: controllerAboutMe,
+                          onChanged: (value) {
+                            aboutMe = value;
+                          },
+                          focusNode: focusNodeAboutMe,
+                        ),
+                      ),
+                      margin: EdgeInsets.only(left: 30.0, right: 30.0),
+                    ),
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
                 ),
-                margin: EdgeInsets.only(top: 50.0, bottom: 50.0),
-              ),
-            ],
+
+                // Button
+                Container(
+                  child: FlatButton(
+                    onPressed: handleUpdateData,
+                    child: Text(
+                      'Xác nhận',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    color: primaryColor,
+                    highlightColor: Color(0xff8d93a0),
+                    splashColor: Colors.transparent,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
+                  ),
+                  margin: EdgeInsets.only(top: 50.0, bottom: 50.0),
+                ),
+              ],
+            ),
           ),
           padding: EdgeInsets.only(left: 15.0, right: 15.0),
         ),
