f3f17fc56b4bd5f78e4078d3f0cd1616e6c28f31
Đặng Đức Mạnh <dmanh120419@gmail.com>
2020-12-24 23:57:53 +0700

    add logic process friend

---
+++ lib/blocs/process_friend_bloc/process_friend_event.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter_snap_chat/models/contact_model.dart';
+
+abstract class ProcessFriendEvent extends Equatable{
+
+}
+class ProcessFriendDataChanged extends ProcessFriendEvent{
+  final List<ContactModel> contacts;
+
+  ProcessFriendDataChanged(this.contacts);
+  @override
+  // TODO: implement props
+  List<Object> get props => [contacts];
+}
